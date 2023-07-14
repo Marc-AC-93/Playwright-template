84722e05e08679f8537d0c1cfaa270fe8332eaf5
Marc_AC <marc.aroca@avios.com>
2023-07-14 13:22:50 +0200

    improve script

--- runDockerCompose.sh
+++ runDockerCompose.sh
@@ -9,9 +9,8 @@ rm -rf playwright-report/
 rm -rf test-results/
 rm -rf allure-report/
 rm -rf allure-results/
-echo $config
 docker compose -f "docker-compose.yml" up -d --build
-docker exec -it playwright bash -c "npm run $config"
+docker exec playwright npm run $config
 docker cp playwright:/playwright/test-results .
 docker cp playwright:/playwright/playwright-report .
 docker cp playwright:/playwright/allure-results .

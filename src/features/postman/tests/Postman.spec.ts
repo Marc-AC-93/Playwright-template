15cc3eaa13b0e0aaebe5e3c29a2bfbcc3523a45b
Marc_AC <marc.aroca@avios.com>
2023-11-03 12:53:27 +0100

    Removed not needed import

--- src/features/postman/tests/Postman.spec.ts
+++ src/features/postman/tests/Postman.spec.ts
@@ -1,5 +1,4 @@
 import { test } from "../helpers/PostmanFixture"
-import {Language} from "../../../common/testData/testConfig/Language";
 
 test.describe( "Postman", ()=>{
     test('Echo post - OK @api', async ({ verifications, postmanApi }) => {
